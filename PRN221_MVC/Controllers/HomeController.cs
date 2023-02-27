a7faca17f9c177034e0c1572d313cab1ca93a7ac
Manh3012 <vuvanmanh3012@gmail.com>
2023-02-27 16:58:26 +0700

    Arrange Folder and Name file

--- PRN221_MVC/Controllers/HomeController.cs
+++ PRN221_MVC/Controllers/HomeController.cs
@@ -17,18 +17,9 @@ namespace PRN221_MVC.Controllers
         {
             return View();
         }
-        public IActionResult WishList()
-        {
-            return View();
-        }
-        public IActionResult Cart()
-        {
-            return View();
-        }
-        public IActionResult CheckOut()
-        {
-            return View();
-        }
+      
+       
+       
 
         public IActionResult LoginClient()
         {
