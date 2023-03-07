a06d5191b7f11a7440af1cb6b722cc2db52b9572
Manh3012 <vuvanmanh3012@gmail.com>
2023-03-07 16:22:17 +0700

    fix Sort

--- PRN221_MVC/Controllers/ProductController.cs
+++ PRN221_MVC/Controllers/ProductController.cs
@@ -112,9 +112,10 @@ namespace PRN221_MVC.Controllers
 
         public ActionResult SearchProduct(string? search)
         {
+            List<Product> items = new List<Product>();
+
             try
             {
-                List<Product> items = new List<Product>();
 
                 items = productService.Search(search.Trim());
                 if (items.Count == 0)
@@ -132,12 +133,14 @@ namespace PRN221_MVC.Controllers
             }
             catch
             {
-                return RedirectToAction("Index", "Home");
+                items=_dbContext.Product.ToList();
+                ViewBag.Search = items;
+                return View("Filter",items);
             }
             return View("Filter");
         }
 
-        public ActionResult Filter(int sortId, int id)
+        public ActionResult Filter(int sortId, int id,List<Product> list)
         {
             var products = productService.Filter(id);
             if (products.Count > 0)
@@ -150,6 +153,7 @@ namespace PRN221_MVC.Controllers
                 ViewBag.Error = "No Item Found";
             }
             List<Product> sort = new List<Product>();
+            List<Product> listProduct = _dbContext.Product.ToList();
             Category category = new Category();
             var cate = _dbContext.Category.FirstOrDefault(x => x.ID == id);
             ViewBag.NameCategory = cate.Name;
@@ -193,49 +197,63 @@ namespace PRN221_MVC.Controllers
             return View(sort);
         }
         [HttpGet]
-        public IActionResult Sort(int sortId, int id)
+        public IActionResult Sort(int sortId)
         {
-            List<Product> sort = new List<Product>();
-            Category category = new Category();
 
-            if (id == 0)
-            {
-                return NotFound();
-            }
+            List<Product> sort = new List<Product>();
+            //switch (sortId)
+            //{
+
+            //    case 1:
+            //           sort =list.OrderBy(x=>x.Name).ToList();
+            //        break;
+            //    case 2:
+            //           sort =list.OrderByDescending(x=>x.Name).ToList();
+
+            //        break;
+            //    case 3:
+            //           sort =list.OrderBy(x=>x.Price).ToList();
+
+            //        break;
+            //    case 4:
+            //           sort =list.OrderByDescending(x=>x.Price).ToList();
+
+            //        break;
+            //    default:
+            //        sort = list.ToList();
+            //        break;
+
+
+            //}
             switch (sortId)
             {
 
                 case 1:
-                    sort = _dbContext.Product.Include(x => x.Category).Where(x => x.Category.ID == id).OrderBy(x => x.Name).ToList();
+                    sort = _dbContext.Product.OrderBy(x => x.Name).ToList();
                     ViewBag.TotalProduct = sort.Count();
                     break;
                 case 2:
-                    sort = _dbContext.Product.Include(x => x.Category).Where(x => x.Category.ID == id).OrderByDescending(x => x.Name).ToList();
+                    sort = _dbContext.Product.OrderByDescending(x => x.Name).ToList();
                     ViewBag.TotalProduct = sort.Count();
                     break;
                 case 3:
-                    sort = _dbContext.Product.Include(x => x.Category).Where(x => x.Category.ID == id).OrderBy(x => x.Price).ToList();
+                    sort = _dbContext.Product.OrderBy(x => x.Price).ToList();
                     ViewBag.TotalProduct = sort.Count();
                     break;
                 case 4:
-                    sort = _dbContext.Product.Include(x => x.Category).Where(x => x.Category.ID == id).OrderByDescending(x => x.Price).ToList();
+                    sort = _dbContext.Product.OrderByDescending(x => x.Price).ToList();
                     ViewBag.TotalProduct = sort.Count();
                     break;
                 default:
-                    sort = _dbContext.Product.Include(x => x.Category).Where(x => x.Category.ID == id).ToList();
-                    category = _dbContext.Category.FirstOrDefault(x => x.ID == id);
-                    foreach (var item in sort)
-                    {
-                        item.Category = category;
-                    }
+                    sort = _dbContext.Product.ToList();
                     ViewBag.TotalProduct = sort.Count();
                     break;
 
 
             }
+            ViewBag.Search = sort;
 
-
-            return View(sort);
+            return View("Filter");
         }
 
         public ActionResult ProSort()
